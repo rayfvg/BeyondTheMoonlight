cbdc9018979026e6a35ae311b0eb37e6cd73c2e6
Aleksandr <rayfvg@yandex.ru>
2024-05-21 13:19:40 +0300

    исправил мелкие баги

--- BeyondTheMoonlight/Assets/Scripts/SpawnerCoin.cs
+++ BeyondTheMoonlight/Assets/Scripts/SpawnerCoin.cs
@@ -9,11 +9,17 @@ public class SpawnerCoin : MonoBehaviour
     [SerializeField] private float _jumpForse;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        
+
         if (collision.gameObject.GetComponent<Move>())
         {
-            Instantiate(_prefabMoney, _spawnCoinTransform.position, Quaternion.identity);
-            _rb.AddForce(Vector2.up * _jumpForse, ForceMode2D.Impulse);
-            gameObject.SetActive(false);
+            float dot = Vector2.Dot(collision.contacts[0].normal, Vector2.down);
+            if (dot > 0.7f)
+            {
+                Instantiate(_prefabMoney, _spawnCoinTransform.position, Quaternion.identity);
+                _rb.AddForce(Vector2.up * _jumpForse, ForceMode2D.Impulse);
+                gameObject.SetActive(false);
+            }
         }
     }
 }
